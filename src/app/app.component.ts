54814fb46d8d5c57e8b79dc4bf8d4c7accc536d9
fffarzan <far.ned.me@gmail.com>
2020-05-18 11:13:56 +0430

    Advanced Transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,11 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   /**
-   * `transition`: Describes how to transition from one state to other.
+   * Note: transition('normal <=> highlighted', animate(300)):
+   * Is the other way of using transition.
+   * 
+   * Note: transition('shrunken <=> *', animate(400)) :
+   * It means 'from any state to shruken' or 'from shrunken to any state'.
    */
   animations: [
     trigger('divState', [
@@ -15,20 +19,43 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         'transform': 'translateX(0)'
       })),
       state('highlighted', style({
-        backgroundColor: 'blue',
-        transform: 'translateX(100px)'
+        'background-color': 'blue',
+        'transform': 'translateX(100px)'
       })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(500))
+      transition('highlighted => normal', animate(300)),
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        'transform': 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        'transform': 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        'transform': 'translateX(0) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(500)),
+      transition('shrunken <=> *', animate(600)),
     ])
   ]
 })
 export class AppComponent {
   state: string = 'normal';
+  wildState: string = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAnimate() {
     this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 
   onAdd(item) {
